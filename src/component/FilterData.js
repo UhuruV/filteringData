b08717273a4c8b877ea290dc9fbcffa0cc970df1
UhuruV <uhuruvalary@gmail.com>
2021-11-04 12:28:48 +0300

    sending a skeleton of the filtering app

---
+++ src/component/FilterData.js
@@ -0,0 +1,13 @@
+import React, { useState } from 'react';
+
+const FilterData = () => {
+    const [fetchedData, setFetchedData] = useState([]);
+    const [filteredData, setFilteredData] = useState([]);
+    return (
+        <div>
+            Component!
+        </div>
+    );
+};
+
+export default FilterData;
